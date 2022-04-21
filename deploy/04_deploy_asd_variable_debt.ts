1f8a1b44c8c554ca975195864b7e8bc008424483
Steven Valeri <steven@aave.com>
2022-04-21 14:50:06 -0400

    feat: use config file

--- deploy/04_deploy_asd_variable_debt.ts
+++ deploy/04_deploy_asd_variable_debt.ts
@@ -1,16 +1,19 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 import { aaveMarketAddresses } from '../src/helpers/aave-v2-addresses';
+import { anteiConfiguration } from '../src/configs/AnteiConfiguration';
 
 const func: DeployFunction = async function ({ getNamedAccounts, deployments, ...hre }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const { pool, treasury, incentivesController } = aaveMarketAddresses;
+  const { pool, incentivesController } = aaveMarketAddresses;
   const asd = await hre.ethers.getContract('AnteiStableDollarEntities');
+
+  const { TOKEN_NAME, TOKEN_SYMBOL } = anteiConfiguration.tokenConfig;
 
   const variableDebtImplementation = await deploy('VariableDebtToken', {
     from: deployer,
-    args: [pool, asd.address, 'Antei Stable Dollar', 'ASD', incentivesController],
+    args: [pool, asd.address, TOKEN_NAME, TOKEN_SYMBOL, incentivesController],
   });
 
   console.log(`Variable Debt Implementation:  ${variableDebtImplementation.address}`);
