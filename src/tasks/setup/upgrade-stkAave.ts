5af38f1022c1eb775283f59d542cd397b115ebb3
Steven Valeri <steven@aave.com>
2022-06-06 16:36:28 -0400

    feat: reduce setup logging

--- src/tasks/setup/upgrade-stkAave.ts
+++ src/tasks/setup/upgrade-stkAave.ts
@@ -12,18 +12,6 @@ task('upgrade-stkAave', 'Upgrade Staked Aave').setAction(async (_, hre) => {
   await hre.run('set-DRE');
   const { ethers } = DRE;
 
-  const stkAaveProxyAsStkAave = await getStakedAave(helperAddresses.stkAave);
-
-  const previousRevision = await stkAaveProxyAsStkAave.REVISION();
-  const previousImplementationAsBytes = await ethers.provider.send('eth_getStorageAt', [
-    helperAddresses.stkAave,
-    '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
-    'latest',
-  ]);
-  const previousimplementationAddress = ethers.utils.getAddress(
-    ethers.utils.hexDataSlice(previousImplementationAsBytes, 12)
-  );
-
   const newStakedAaveImpl = await ethers.getContract('StakedTokenV2Rev4');
 
   const governanceSigner = await impersonateAccountHardhat(helperAddresses.longExecutor);
@@ -42,7 +30,6 @@ task('upgrade-stkAave', 'Upgrade Staked Aave').setAction(async (_, hre) => {
     anteiVariableDebtTokenAddress,
   ]);
 
-  let error = false;
   console.log(`trying to upgrade stkAave...`);
   const upgradeTx = await stkAaveProxy.upgradeToAndCall(
     newStakedAaveImpl.address,
@@ -54,7 +41,4 @@ task('upgrade-stkAave', 'Upgrade Staked Aave').setAction(async (_, hre) => {
     .withArgs(newStakedAaveImpl.address);
 
   console.log(`StkAave implementation set to: ${newStakedAaveImpl.address}`);
-  console.log(`Previous Implementation: ${previousimplementationAddress}`);
-  console.log(`Previous Revision ${previousRevision}`);
-  console.log(`New Revision ${await stkAaveProxyAsStkAave.REVISION()}`);
 });
