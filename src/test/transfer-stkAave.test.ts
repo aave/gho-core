1c6c86ed2edd34b8570eca58c42b915e20cfaf69
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-07-05 12:39:03 +0200

    test: Fix transfer stkAave tests

--- src/test/transfer-stkAave.test.ts
+++ src/test/transfer-stkAave.test.ts
@@ -2,10 +2,11 @@ import { expect } from 'chai';
 import { BigNumber } from 'ethers';
 import './helpers/math/wadraymath';
 import { makeSuite, TestEnv } from './helpers/make-suite';
-import { DRE, timeLatest, setBlocktime, mine } from '../helpers/misc-utils';
-import { ONE_YEAR, MAX_UINT, WAD } from '../helpers/constants';
-import { asdReserveConfig, aaveMarketAddresses } from '../helpers/config';
-import { calcCompoundedInterestV2 } from './helpers/math/calculations';
+import { DRE, setBlocktime } from '../helpers/misc-utils';
+import { ONE_YEAR, PERCENTAGE_FACTOR, ZERO_ADDRESS } from '../helpers/constants';
+import { asdReserveConfig } from '../helpers/config';
+import { calcCompoundedInterestV2, calcDiscountRate } from './helpers/math/calculations';
+import { getTxCostAndTimestamp } from './helpers/helpers';
 
 makeSuite('Antei StkAave Transfer', (testEnv: TestEnv) => {
   let ethers;
@@ -13,13 +14,11 @@ makeSuite('Antei StkAave Transfer', (testEnv: TestEnv) => {
   let collateralAmount;
   let borrowAmount;
 
-  let startTime;
   let oneYearLater;
 
-  let user1Signer;
-  let user1Address;
-  let user2Signer;
-  let user2Address;
+  let rcpt, tx;
+
+  let discountRate, asdDiscountedPerDiscountToken, minDiscountTokenBalance;
 
   before(async () => {
     ethers = DRE.ethers;
@@ -27,49 +26,89 @@ makeSuite('Antei StkAave Transfer', (testEnv: TestEnv) => {
     collateralAmount = ethers.utils.parseUnits('1000.0', 18);
     borrowAmount = ethers.utils.parseUnits('1000.0', 18);
 
-    const { users } = testEnv;
-    user1Signer = users[0].signer;
-    user1Address = users[0].address;
-    user2Signer = users[1].signer;
-    user2Address = users[1].address;
+    const { users, discountRateStrategy } = testEnv;
+    users[0].signer = users[0].signer;
+    users[0].address = users[0].address;
+    users[1].signer = users[1].signer;
+    users[1].address = users[1].address;
+
+    // Fetch discount rate strategy parameters
+    [discountRate, asdDiscountedPerDiscountToken, minDiscountTokenBalance] = await Promise.all([
+      discountRateStrategy.DISCOUNT_RATE(),
+      discountRateStrategy.ASD_DISCOUNTED_PER_DISCOUNT_TOKEN(),
+      discountRateStrategy.MIN_DISCOUNT_TOKEN_BALANCE(),
+    ]);
   });
 
   it('Transfer from user with stkAave and asd to user without asd', async function () {
     // setup
-    const { pool, weth, asd, variableDebtToken } = testEnv;
+    const { users, pool, weth, asd, variableDebtToken } = testEnv;
 
     const { stakedAave, stkAaveWhale } = testEnv;
     const stkAaveAmount = ethers.utils.parseUnits('10.0', 18);
-    await stakedAave.connect(stkAaveWhale.signer).transfer(user1Address, stkAaveAmount);
+    await stakedAave.connect(stkAaveWhale.signer).transfer(users[0].address, stkAaveAmount);
 
-    await weth.connect(user1Signer).approve(pool.address, collateralAmount);
-    await pool.connect(user1Signer).deposit(weth.address, collateralAmount, user1Address, 0);
-    await pool.connect(user1Signer).borrow(asd.address, borrowAmount, 2, 0, user1Address);
+    await weth.connect(users[0].signer).approve(pool.address, collateralAmount);
+    await pool
+      .connect(users[0].signer)
+      .deposit(weth.address, collateralAmount, users[0].address, 0);
+    await pool.connect(users[0].signer).borrow(asd.address, borrowAmount, 2, 0, users[0].address);
 
-    const poolData = await pool.getReserveData(asd.address);
+    const { lastUpdateTimestamp, variableBorrowIndex } = await pool.getReserveData(asd.address);
 
-    startTime = BigNumber.from(poolData.lastUpdateTimestamp);
-    const variableBorrowIndex = poolData.variableBorrowIndex;
+    const user1ScaledBefore = await variableDebtToken.scaledBalanceOf(users[0].address);
 
-    oneYearLater = startTime.add(BigNumber.from(ONE_YEAR));
+    // Updating the timestamp for the borrow to be one year later
+    oneYearLater = BigNumber.from(lastUpdateTimestamp).add(BigNumber.from(ONE_YEAR));
     await setBlocktime(oneYearLater.toNumber());
-    await mine(); // Mine block to increment time in underlying chain as well
+
+    const user1DiscountPercentBefore = await variableDebtToken.getDiscountPercent(users[0].address);
 
     // calculate expected results
-    await expect(stakedAave.connect(user1Signer).transfer(user2Address, stkAaveAmount))
-      .to.emit(variableDebtToken, 'Transfer')
-      .to.emit(variableDebtToken, 'Burn');
-
+    tx = await stakedAave.connect(users[0].signer).transfer(users[1].address, stkAaveAmount);
+    rcpt = await tx.wait();
+    const { txTimestamp } = await getTxCostAndTimestamp(rcpt);
     const multiplier = calcCompoundedInterestV2(
       asdReserveConfig.INTEREST_RATE,
-      oneYearLater.add(1),
-      startTime
+      txTimestamp,
+      BigNumber.from(lastUpdateTimestamp)
     );
     const expIndex = variableBorrowIndex.rayMul(multiplier);
-    const user1Scaled = await variableDebtToken.scaledBalanceOf(user1Address);
-    const user1ExpectedBalance = user1Scaled.rayMul(expIndex);
-    const user1Debt = await variableDebtToken.balanceOf(user1Address);
 
-    expect(user1Debt).to.be.eq(user1ExpectedBalance);
+    const user1ExpectedBalanceNoDiscount = user1ScaledBefore.rayMul(expIndex);
+    const user1BalanceIncrease = user1ExpectedBalanceNoDiscount.sub(borrowAmount);
+    const user1ExpectedDiscount = user1BalanceIncrease
+      .mul(user1DiscountPercentBefore)
+      .div(PERCENTAGE_FACTOR);
+    const user1ExpectedBalance = user1ExpectedBalanceNoDiscount.sub(user1ExpectedDiscount);
+    const user1BalanceIncreaseWithDiscount = user1BalanceIncrease.sub(user1ExpectedDiscount);
+
+    const user1DiscountTokenBalance = await stakedAave.balanceOf(users[0].address);
+    const user1ExpectedDiscountPercent = calcDiscountRate(
+      discountRate,
+      asdDiscountedPerDiscountToken,
+      minDiscountTokenBalance,
+      user1ExpectedBalance,
+      user1DiscountTokenBalance
+    );
+
+    await expect(tx)
+      .to.emit(variableDebtToken, 'Transfer')
+      .withArgs(users[0].address, ZERO_ADDRESS, user1BalanceIncreaseWithDiscount)
+      .to.emit(variableDebtToken, 'Burn')
+      .withArgs(
+        users[0].address,
+        ZERO_ADDRESS,
+        user1BalanceIncreaseWithDiscount,
+        user1BalanceIncreaseWithDiscount,
+        expIndex
+      )
+      .to.emit(variableDebtToken, 'DiscountAppliedToDebt')
+      .withArgs(users[0].address, user1ExpectedDiscount)
+      .to.emit(variableDebtToken, 'DiscountPercentUpdated')
+      .withArgs(users[0].address, user1DiscountPercentBefore, user1ExpectedDiscountPercent);
+
+    const user1Debt = await variableDebtToken.balanceOf(users[0].address);
+    expect(user1Debt).to.be.closeTo(user1ExpectedBalance, 1);
   });
 });
