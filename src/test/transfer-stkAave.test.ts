2689df6c63666ed4f9331f1c81767f4c921e8b4e
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-07-06 23:14:23 +0200

    test: Fix errors from merge conflicts

--- src/test/transfer-stkAave.test.ts
+++ src/test/transfer-stkAave.test.ts
@@ -2,12 +2,13 @@ import { expect } from 'chai';
 import { BigNumber } from 'ethers';
 import './helpers/math/wadraymath';
 import { makeSuite, TestEnv } from './helpers/make-suite';
-import { DRE, timeLatest, setBlocktime, mine } from '../helpers/misc-utils';
-import { ONE_YEAR, MAX_UINT, WAD } from '../helpers/constants';
-import { ghoReserveConfig, aaveMarketAddresses } from '../helpers/config';
-import { calcCompoundedInterestV2 } from './helpers/math/calculations';
+import { DRE, setBlocktime } from '../helpers/misc-utils';
+import { ONE_YEAR, PERCENTAGE_FACTOR, ZERO_ADDRESS } from '../helpers/constants';
+import { ghoReserveConfig } from '../helpers/config';
+import { calcCompoundedInterestV2, calcDiscountRate } from './helpers/math/calculations';
+import { getTxCostAndTimestamp } from './helpers/helpers';
 
-makeSuite('Gho StkAave Transfer', (testEnv: TestEnv) => {
+makeSuite('Antei StkAave Transfer', (testEnv: TestEnv) => {
   let ethers;
 
   let collateralAmount;
@@ -17,7 +18,7 @@ makeSuite('Gho StkAave Transfer', (testEnv: TestEnv) => {
 
   let rcpt, tx;
 
-  let discountRate, asdDiscountedPerDiscountToken, minDiscountTokenBalance;
+  let discountRate, ghoDiscountedPerDiscountToken, minDiscountTokenBalance;
 
   before(async () => {
     ethers = DRE.ethers;
@@ -32,26 +33,28 @@ makeSuite('Gho StkAave Transfer', (testEnv: TestEnv) => {
     users[1].address = users[1].address;
 
     // Fetch discount rate strategy parameters
-    [discountRate, asdDiscountedPerDiscountToken, minDiscountTokenBalance] = await Promise.all([
+    [discountRate, ghoDiscountedPerDiscountToken, minDiscountTokenBalance] = await Promise.all([
       discountRateStrategy.DISCOUNT_RATE(),
-      discountRateStrategy.ASD_DISCOUNTED_PER_DISCOUNT_TOKEN(),
+      discountRateStrategy.GHO_DISCOUNTED_PER_DISCOUNT_TOKEN(),
       discountRateStrategy.MIN_DISCOUNT_TOKEN_BALANCE(),
     ]);
   });
 
-  it('Transfer from user with stkAave and gho to user without gho', async function () {
+  it('Transfer from user with stkAave and GHO to user without GHO', async function () {
     // setup
-    const { pool, weth, gho, variableDebtToken } = testEnv;
+    const { users, pool, weth, gho, variableDebtToken } = testEnv;
 
     const { stakedAave, stkAaveWhale } = testEnv;
     const stkAaveAmount = ethers.utils.parseUnits('10.0', 18);
     await stakedAave.connect(stkAaveWhale.signer).transfer(users[0].address, stkAaveAmount);
 
-    await weth.connect(user1Signer).approve(pool.address, collateralAmount);
-    await pool.connect(user1Signer).deposit(weth.address, collateralAmount, user1Address, 0);
-    await pool.connect(user1Signer).borrow(gho.address, borrowAmount, 2, 0, user1Address);
+    await weth.connect(users[0].signer).approve(pool.address, collateralAmount);
+    await pool
+      .connect(users[0].signer)
+      .deposit(weth.address, collateralAmount, users[0].address, 0);
+    await pool.connect(users[0].signer).borrow(gho.address, borrowAmount, 2, 0, users[0].address);
 
-    const poolData = await pool.getReserveData(gho.address);
+    const { lastUpdateTimestamp, variableBorrowIndex } = await pool.getReserveData(gho.address);
 
     const user1ScaledBefore = await variableDebtToken.scaledBalanceOf(users[0].address);
 
@@ -70,8 +73,8 @@ makeSuite('Gho StkAave Transfer', (testEnv: TestEnv) => {
     const { txTimestamp } = await getTxCostAndTimestamp(rcpt);
     const multiplier = calcCompoundedInterestV2(
       ghoReserveConfig.INTEREST_RATE,
-      oneYearLater.add(1),
-      startTime
+      txTimestamp,
+      BigNumber.from(lastUpdateTimestamp)
     );
     const expIndex = variableBorrowIndex.rayMul(multiplier);
 
@@ -86,7 +89,7 @@ makeSuite('Gho StkAave Transfer', (testEnv: TestEnv) => {
     const user1DiscountTokenBalance = await stakedAave.balanceOf(users[0].address);
     const user1ExpectedDiscountPercent = calcDiscountRate(
       discountRate,
-      asdDiscountedPerDiscountToken,
+      ghoDiscountedPerDiscountToken,
       minDiscountTokenBalance,
       user1ExpectedBalance,
       user1DiscountTokenBalance
