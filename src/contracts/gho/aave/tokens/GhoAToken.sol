7de818e4258357486b8b2df4251c6b462ca6e0ab
Steven Valeri <steven@aave.com>
2022-07-09 17:32:25 -0400

    fix: revert ILendingPoolAddressesProvider update

--- src/contracts/gho/aave/tokens/GhoAToken.sol
+++ src/contracts/gho/aave/tokens/GhoAToken.sol
@@ -56,7 +56,9 @@ contract GhoAToken is VersionedInitializable, IncentivizedERC20, IGhoAToken {
    * @dev Only pool admin can call functions marked by this modifier.
    **/
   modifier onlyLendingPoolAdmin() {
-    ILendingPoolAddressesProvider addressesProvider = POOL.getAddressesProvider();
+    ILendingPoolAddressesProvider addressesProvider = ILendingPoolAddressesProvider(
+      ADDRESSES_PROVIDER
+    );
     require(addressesProvider.getPoolAdmin() == msg.sender, Errors.CALLER_NOT_POOL_ADMIN);
     _;
   }
@@ -73,6 +75,7 @@ contract GhoAToken is VersionedInitializable, IncentivizedERC20, IGhoAToken {
     POOL = pool;
     UNDERLYING_ASSET_ADDRESS = underlyingAssetAddress;
     RESERVE_TREASURY_ADDRESS = reserveTreasuryAddress;
+    ADDRESSES_PROVIDER = addressesProvider;
   }
 
   function getRevision() internal pure virtual override returns (uint256) {
