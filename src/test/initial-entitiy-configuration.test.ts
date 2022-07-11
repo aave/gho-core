53851667806d972df5f6c6cd294245004731bae9
The-3D <frangellaemilio@gmail.com>
2022-07-11 21:13:22 +0200

    feat: initial deployment script fix

--- src/test/initial-entitiy-configuration.test.ts
+++ src/test/initial-entitiy-configuration.test.ts
@@ -13,18 +13,11 @@ makeSuite('Initial GHO Aave Entity Configuration', (testEnv: TestEnv) => {
 
   it('Aave entity data check', async function () {
     const { gho, aToken, variableDebtToken } = testEnv;
-    const aaveEntity = await gho.getEntityById(1);
+    const aaveFacilitator = await gho.getFacilitator(aToken.address);
 
-    const { label, entityAddress, mintLimit, mintBalance, minters, burners, active } = aaveEntity;
+    const { label, bucket } = aaveFacilitator;
 
     expect(label).to.be.equal(ghoEntityConfig.label);
-    expect(entityAddress).to.be.equal(ZERO_ADDRESS);
-    expect(mintLimit).to.be.equal(ghoEntityConfig.mintLimit);
-    expect(mintBalance).to.be.equal(0);
-    expect(minters.length).to.be.equal(1);
-    expect(minters[0]).to.be.equal(aToken.address);
-    expect(burners.length).to.be.equal(1);
-    expect(burners[0]).to.be.equal(aToken.address);
-    expect(active).to.be.true;
+    expect(bucket.maxCapacity).to.be.equal(ghoEntityConfig.mintLimit);
   });
 });
