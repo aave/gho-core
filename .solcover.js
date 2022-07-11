16ef0247796b4c9aaa8bce3150c0f23e48710e67
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-07-11 10:59:04 +0200

    fix: Fix solcover file

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,4 @@
-const accounts = require(`./src/helpers/test-wallets.ts`).accounts;
-const cp = require('child_process');
-
 module.exports = {
-  client: require('ganache-cli'),
   skipFiles: ['dependencies/', 'interfaces/', 'poolUpgrade/', 'stkAaveUpgrade/'],
   mocha: {
     enableTimeouts: false,
