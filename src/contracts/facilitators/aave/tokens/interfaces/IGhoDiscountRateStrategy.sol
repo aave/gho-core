ebc73b25aa207a422cd12e27225e81671d606a4f
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-10-13 12:57:34 +0200

    docs: Fix docs of DiscountRateStrategy

--- src/contracts/facilitators/aave/tokens/interfaces/IGhoDiscountRateStrategy.sol
+++ src/contracts/facilitators/aave/tokens/interfaces/IGhoDiscountRateStrategy.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.10;
 
+/**
+ * @title IGhoDiscountRateStrategy
+ * @author Aave
+ * @notice Defines the basic interface of the GhoDiscountRateStrategy
+ */
 interface IGhoDiscountRateStrategy {
   /**
-   * @dev Calculates the interest rates depending on the reserve's state and configurations
-   * @param debtBalance The address of the reserve
-   * @param discountTokenBalance The liquidity available in the reserve
+   * @notice Calculates the discount rate depending on the debt and discount token balances
+   * @param debtBalance The debt balance of the user
+   * @param discountTokenBalance The discount token balance of the user
    * @return The discount rate, as a percentage - the maximum can be 10000 = 100.00%
    **/
   function calculateDiscountRate(uint256 debtBalance, uint256 discountTokenBalance)
