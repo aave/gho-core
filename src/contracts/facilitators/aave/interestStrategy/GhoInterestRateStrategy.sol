3979bfa97118cb73fd173c5ebdb81ab248f407c5
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-10-17 11:05:16 +0200

    docs: Fix IR docs

--- src/contracts/facilitators/aave/interestStrategy/GhoInterestRateStrategy.sol
+++ src/contracts/facilitators/aave/interestStrategy/GhoInterestRateStrategy.sol
@@ -8,7 +8,7 @@ import {IReserveInterestRateStrategy} from '@aave/core-v3/contracts/interfaces/I
  * @title GhoInterestRateStrategy
  * @author Aave
  * @notice Implements the calculation of GHO interest rates
- * @dev The variable borrow interest rate is fixed at 1.00%.
+ * @dev The variable borrow interest rate is fixed at deployment time.
  **/
 contract GhoInterestRateStrategy is IReserveInterestRateStrategy {
   // Variable borrow rate (expressed in ray)
