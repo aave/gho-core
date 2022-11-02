d7a0f295c1cff8ea7940a37a56251923203c3826
Steven Valeri <steven@aave.com>
2022-11-02 15:14:41 -0400

    fix: increment flashminter solidity version

--- src/contracts/facilitators/flashMinter/GhoFlashMinter.sol
+++ src/contracts/facilitators/flashMinter/GhoFlashMinter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity ^0.8.0;
+pragma solidity 0.8.10;
 
 import {IACLManager} from '@aave/core-v3/contracts/interfaces/IACLManager.sol';
 import {PoolAddressesProvider} from '@aave/core-v3/contracts/protocol/configuration/PoolAddressesProvider.sol';
