22ee8c7865ddf0d2d5c093519cddf98115ee71be
Steven Valeri <steven@aave.com>
2022-11-03 15:51:17 -0400

    fix: align version numbers

--- src/contracts/facilitators/aave/tokens/interfaces/IGhoVariableDebtToken.sol
+++ src/contracts/facilitators/aave/tokens/interfaces/IGhoVariableDebtToken.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.8.10;
-
+pragma solidity ^0.8.0;
 import {IVariableDebtToken} from '@aave/core-v3/contracts/interfaces/IVariableDebtToken.sol';
 
 /**
