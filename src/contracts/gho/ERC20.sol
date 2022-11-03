3799c1da7edc40c9f5af0cf943e10fc41ad34331
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-11-03 10:36:54 +0100

    docs: Remove unneeded docs

--- src/contracts/gho/ERC20.sol
+++ src/contracts/gho/ERC20.sol
@@ -3,11 +3,6 @@ pragma solidity >=0.8.0;
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
-/// @notice Modern and gas efficient ERC20 + EIP-2612 implementation.
-/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC20.sol)
-/// @author Modified from Uniswap (https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2ERC20.sol)
-/// @dev Do not manually set balances without updating totalSupply, as the sum of all user balances must not exceed it.
-
 /**
  * @title ERC20
  * @notice Gas Efficient ERC20 + EIP-2612 implementation
