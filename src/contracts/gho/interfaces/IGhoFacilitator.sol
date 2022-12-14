c69fd51984bff7b50a51c07c289048ff9e1940fe
Steven Valeri <steven@aave.com>
2022-12-14 07:52:21 -0500

    fix: update natspec comment

--- src/contracts/gho/interfaces/IGhoFacilitator.sol
+++ src/contracts/gho/interfaces/IGhoFacilitator.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.0;
 /**
  * @title IGhoFacilitator
  * @author Aave
- * @notice Defines the behavior of a GhoFacilitator
+ * @notice Defines the behavior of a Gho Facilitator
  */
 interface IGhoFacilitator {
   /**
-   * @dev Emitted fees are distributed to the GhoTreasury
+   * @dev Emitted when fees are distributed to the GhoTreasury
    * @param ghoTreasury The address of the ghoTreasury
    * @param amount The amount of Gho transferred to the ghoTreasury
    */
   event FeesDistributedToTreasury(address indexed ghoTreasury, uint256 amount);
 
   /**
-   * @notice Distribute accumulated fees to the GHO treasury
+   * @notice Distribute accumulated fees to the GhoTreasury
    */
   function distributeFeesToTreasury() external;
 }
