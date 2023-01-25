52e96330aa2fa0482e4a8daba1218b0ba5e7785a
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-01-25 18:44:29 +0000

    Add MIT license across contracts (#239)
    * add licenses across contracts
    * fix: Revert license change of StakedAave implementation

--- src/contracts/facilitators/aave/interestStrategy/GhoDiscountRateStrategy.sol
+++ src/contracts/facilitators/aave/interestStrategy/GhoDiscountRateStrategy.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
 import {WadRayMath} from '@aave/core-v3/contracts/protocol/libraries/math/WadRayMath.sol';
