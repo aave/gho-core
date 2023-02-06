e5646951c16852c212f10986acb6f02d0e56e6aa
David Racero <canillas.mail@gmail.com>
2023-02-06 10:26:46 +0100

    fix: Import and reuse error codes at test suite

---
+++ src/test/helpers/constants.ts
@@ -0,0 +1,4 @@
+export const INITIALIZED = 'Contract instance has already been initialized';
+export const ZERO_ADDRESS_NOT_VALID = 'ZERO_ADDRESS_NOT_VALID';
+export const CALLER_NOT_DISCOUNT_TOKEN = 'CALLER_NOT_DISCOUNT_TOKEN';
+export const CALLER_NOT_A_TOKEN = 'CALLER_NOT_A_TOKEN';
