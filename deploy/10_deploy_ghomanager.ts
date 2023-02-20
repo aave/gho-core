37e7607ca5c2254a6b4ef03090071fd218d540bc
miguelmtz <36620902+miguelmtzinf@users.noreply.github.com>
2023-02-20 18:04:08 +0100

    fix: Clean up deploy script for GhoManager

--- deploy/10_deploy_ghomanager.ts
+++ deploy/10_deploy_ghomanager.ts
@@ -1,15 +1,11 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { getPoolAddressesProvider } from '@aave/deploy-v3';
-
 const func: DeployFunction = async function ({
   getNamedAccounts,
   deployments,
 }: HardhatRuntimeEnvironment) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-
-  const addressesProvider = await getPoolAddressesProvider();
 
   const ghoManager = await deploy('GhoManager', {
     from: deployer,
