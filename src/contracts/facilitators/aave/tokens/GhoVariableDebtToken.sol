96fc7f24e25a6fa4c1323c0d3d7714840abe4377
miguelmtzinf <miguelmtz.mail@gmail.com>
2023-03-08 10:16:43 +0100

    fix: Replace error msg on require for consistency

--- src/contracts/facilitators/aave/tokens/GhoVariableDebtToken.sol
+++ src/contracts/facilitators/aave/tokens/GhoVariableDebtToken.sol
@@ -243,7 +243,7 @@ contract GhoVariableDebtToken is DebtTokenBase, ScaledBalanceTokenBase, IGhoVari
 
   /// @inheritdoc IGhoVariableDebtToken
   function updateDiscountToken(address newDiscountToken) external override onlyPoolAdmin {
-    require(newDiscountToken != address(0), 'ZERO_ADDR');
+    require(newDiscountToken != address(0), 'ZERO_ADDRESS_NOT_VALID');
     address oldDiscountToken = address(_discountToken);
     _discountToken = IERC20(newDiscountToken);
     emit DiscountTokenUpdated(oldDiscountToken, newDiscountToken);
