7b7b7df22cb3e557b19329ed9ba7b638b7c10e1f
kartojal <canillas.mail@gmail.com>
2023-03-15 13:13:29 +0100

    chore: remove the need to merge artifacts, and use hardhat dependency compiler for better integration without --no-compile flags

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -2,15 +2,8 @@
 #!/bin/bash
 
 # @dev
-# This bash script setups the needed artifacts to use
-# the @aave/deploy-v3 package as source of deployment
-# scripts for testing or coverage purposes.
-#
-# A separate  artifacts directory was created 
-# due at running tests all external artifacts
-# located at /artifacts are deleted,  causing
-# the deploy library to not find the external
-# artifacts. 
+# This bash script ensures a clean repository
+# and loads environment variables for testing and deploying GHO source code.
 
 echo "[BASH] Setting up testnet enviroment"
 
@@ -23,28 +16,6 @@ if [ ! "$COVERAGE" = true ]; then
 else
     echo "[BASH] Skipping compilation to keep coverage artifacts"
 fi
-
-# Copy artifacts into /artifacts directory to load all artifacts and preventing duplicates 
-
-# Import external @aave/safety-module artifacts
-mkdir -p artifacts/@aave/safety-module/contracts
-mkdir -p artifacts/@aave/safety-module/build-info
-
-cp -r node_modules/@aave/deploy-v3/artifacts/@aave/safety-module/contracts/stake  artifacts/@aave/safety-module/contracts
-cp -r node_modules/@aave/deploy-v3/artifacts/@aave/safety-module/contracts/proposals  artifacts/@aave/safety-module/contracts
-cp -r node_modules/@aave/deploy-v3/artifacts/build-info/*  artifacts/build-info
-
-# Import external @aave/core artifacts
-mkdir -p artifacts/contracts
-
-cp -r node_modules/@aave/core-v3/artifacts/contracts/* artifacts/contracts
-cp -r node_modules/@aave/core-v3/artifacts/build-info/* artifacts/build-info
-
-
-# Import external @aave/periphery artifacts
-cp -r node_modules/@aave/periphery-v3/artifacts/contracts/* artifacts/contracts
-cp -r node_modules/@aave/periphery-v3/artifacts/build-info/*   artifacts/build-info
-
 
 # Export MARKET_NAME variable to use Aave market as testnet deployment setup
 export MARKET_NAME="Test"
