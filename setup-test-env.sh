8221d44caed3ebc69611aa11b28d4c6fcc6090d1
David K <canillas.mail@gmail.com>
2023-03-15 21:26:24 +0100

    fix: Add metadata field to deployment artifacts (#309)
    * fix: keep metadata field at deployment artifacts via using the correct build-info path
    
    * chore: lint package.json
    
    * chore: remove the need to merge artifacts, and use hardhat dependency compiler for better integration without --no-compile flags
    
    * chore: lint

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -2,15 +2,8 @@
 #!/bin/bash
 
 # @dev
-# This bash script setups the needed artifacts to use
-# the @aave/deploy-v3 package as source of deployment
-# scripts for testing or coverage purposes.
-#
-# A separate  artifacts directory was created 
-# due at running tests all external artifacts
-# located at /artifacts are deleted,  causing
-# the deploy library to not find the external
-# artifacts. 
+# This bash script ensures a clean repository
+# and loads environment variables for testing and deploying GHO source code.
 
 echo "[BASH] Setting up testnet enviroment"
 
@@ -23,30 +16,6 @@ if [ ! "$COVERAGE" = true ]; then
 else
     echo "[BASH] Skipping compilation to keep coverage artifacts"
 fi
-
-# Copy artifacts into separate directory to allow
-# the hardhat-deploy library load all artifacts without duplicates 
-mkdir -p temp-artifacts
-cp -r artifacts/@openzeppelin temp-artifacts/
-cp -r artifacts/build-info temp-artifacts/
-cp -r artifacts/src temp-artifacts/
-
-# Import external @aave/safety-module artifacts
-mkdir -p temp-artifacts/safety-module
-cp -r 'node_modules/@aave/deploy-v3/artifacts/@aave/safety-module/contracts/stake' temp-artifacts/safety-module
-cp -r 'node_modules/@aave/deploy-v3/artifacts/@aave/safety-module/contracts/proposals' temp-artifacts/safety-module
-
-# Import external @aave/periphery artifacts
-mkdir -p temp-artifacts/periphery
-cp -r node_modules/@aave/periphery-v3/artifacts/contracts/* temp-artifacts/periphery
-
-# Import external @aave/core artifacts
-mkdir -p temp-artifacts/core-v3
-cp -r node_modules/@aave/core-v3/artifacts/contracts/* temp-artifacts/core-v3
-
-# Import external @aave/deploy artifacts
-mkdir -p temp-artifacts/deploy
-cp -r node_modules/@aave/deploy-v3/artifacts/contracts/* temp-artifacts/deploy
 
 # Export MARKET_NAME variable to use Aave market as testnet deployment setup
 export MARKET_NAME="Test"
