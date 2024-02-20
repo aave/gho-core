effd3616d017232d07c3e7b84a41ac5c727acdeb
miguelmtzinf <miguelmtz.mail@gmail.com>
2024-02-20 11:55:14 +0100

    fix: Add combined coverage script

--- combined-coverage.sh
+++ combined-coverage.sh
@@ -5,14 +5,13 @@
 # This bash script creates coverage reports via Hardhat and Foundry
 # and then merges them, removing uninteresting files
 
-export NODE_OPTIONS="--max_old_space_size=8192"
-
-set -e
-
 npm run hardhat coverage
 forge coverage --report lcov
 
-sed -i -e 's/\/.*gho-core.//g' coverage/lcov.info
+# sed -i -e 's/\/.*gho-core.//g' coverage/lcov.info
+sed -i -e '/^FN\|FNDA:/s/,[^.]*\./,/g' lcov.info
+
+
 
 lcov \
     --rc lcov_branch_coverage=1 \
@@ -24,7 +23,7 @@ lcov \
     --rc lcov_branch_coverage=1 \
     --remove merged-lcov.info \
     --output-file combined-lcov.info \
-    "*node_modules*" "*test*" "*mock*"
+    "*node_modules*" "*test*" "*mock*" "*script*"
 
 rm merged-lcov.info
 
