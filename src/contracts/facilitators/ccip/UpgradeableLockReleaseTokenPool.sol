470211d6d466cd3678dc197d19007df648cd19da
miguelmtzinf <miguelmtz.mail@gmail.com>
2024-04-19 11:22:45 +0200

    fix: Amend gho ccip token pool license

--- src/contracts/facilitators/ccip/UpgradeableLockReleaseTokenPool.sol
+++ src/contracts/facilitators/ccip/UpgradeableLockReleaseTokenPool.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
 import {LockReleaseTokenPool} from '@ccip/src/v0.8/ccip/pools/LockReleaseTokenPool.sol';
