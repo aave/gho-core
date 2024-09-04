b1021361c10d41f74af8307daaaf75b046ed7527
YBM <yan@avara.xyz>
2024-09-04 08:46:01 -0500

    fix: resolve compilation issues

--- src/contracts/facilitators/gsm/GsmConverter.sol
+++ src/contracts/facilitators/gsm/GsmConverter.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.10;
 
 // import {VersionedInitializable} from '@aave/core-v3/contracts/protocol/libraries/aave-upgradeability/VersionedInitializable.sol';
-// import {IERC20} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
+import {IERC20} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
 import {GPv2SafeERC20} from '@aave/core-v3/contracts/dependencies/gnosis/contracts/GPv2SafeERC20.sol';
 // import {EIP712} from '@openzeppelin/contracts/utils/cryptography/EIP712.sol';
 // import {SignatureChecker} from '@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol';
 // import {SafeCast} from '@openzeppelin/contracts/utils/math/SafeCast.sol';
 // import {AccessControl} from '@openzeppelin/contracts/access/AccessControl.sol';
 // import {IGhoFacilitator} from '../../gho/interfaces/IGhoFacilitator.sol';
-// import {IGhoToken} from '../../gho/interfaces/IGhoToken.sol';
+import {IGhoToken} from '../../gho/interfaces/IGhoToken.sol';
 // import {IGsmPriceStrategy} from './priceStrategy/interfaces/IGsmPriceStrategy.sol';
 // import {IGsmFeeStrategy} from './feeStrategy/interfaces/IGsmFeeStrategy.sol';
 import {IGsm} from './interfaces/IGsm.sol';
@@ -25,10 +25,10 @@ contract GsmConverter {
   using GPv2SafeERC20 for IERC20;
 
   address public immutable GSM;
-  address public immutable GHO_TOKEN;
-  address public immutable REDEEMABLE_ASSET; // BUIDL token
-  address public immutable REDEEMED_ASSET; // USDC token
-  address public immutable REDEMPTION; // redeem BUIDL to USDC
+  // address public immutable GHO_TOKEN;
+  address public immutable REDEEMABLE_ASSET;
+  address public immutable REDEEMED_ASSET;
+  address public immutable REDEMPTION_CONTRACT;
 
   event BuyAssetThroughRedemption(
     address indexed originator,
@@ -39,18 +39,24 @@ contract GsmConverter {
 
   /**
    * @dev Constructor
-   * @param ghoToken The address of the GHO token contract
-   * @param underlyingAsset The address of the collateral asset
-   * @param priceStrategy The address of the price strategy
+   * @param gsm The address of the GSM contract associated with conversion
+   * @param redemptionContract The address of the
+   * @param redeemableAsset The address of the
+   * @param redeemedAsset The address of the
    */
-  constructor(address gsm, address redemption, address redeemableAsset, address redeemedAsset) {
+  constructor(
+    address gsm,
+    address redemptionContract,
+    address redeemableAsset,
+    address redeemedAsset
+  ) {
     require(gsm != address(0), 'ZERO_ADDRESS_NOT_VALID');
-    require(redemption != address(0), 'ZERO_ADDRESS_NOT_VALID');
+    require(redemptionContract != address(0), 'ZERO_ADDRESS_NOT_VALID');
     require(redeemableAsset != address(0), 'ZERO_ADDRESS_NOT_VALID');
     require(redeemedAsset != address(0), 'ZERO_ADDRESS_NOT_VALID');
 
     GSM = gsm;
-    REDEMPTION = redemption;
+    REDEMPTION_CONTRACT = redemptionContract;
     REDEEMABLE_ASSET = redeemableAsset;
     REDEEMED_ASSET = redeemedAsset;
   }
@@ -62,6 +68,7 @@ contract GsmConverter {
   // - offramp BUIDL to USDC, send USDC to user
   // - call redeem on offramp to get BUIDL
   // - send USDC to user, safeTransfer
+  // - do we need to use multicall? https://docs.openzeppelin.com/contracts/4.x/utilities#multicall
 
   /**
    * @dev
@@ -70,9 +77,9 @@ contract GsmConverter {
     uint256 minAmount,
     address receiver
   ) external returns (uint256, uint256) {
-    IGhoToken(IGSM(GSM).GHO_TOKEN()).transferFrom(msg.sender, address(this), minAmount);
-    (uint256 redemptionAssetAmount, uint256 ghoSold) = IGSM(GSM).buyAsset(minAmount, receiver);
-    IRedemption(REDEMPTION).redeem(redemptionAssetAmount);
+    IGhoToken(IGsm(GSM).GHO_TOKEN()).transferFrom(msg.sender, address(this), minAmount);
+    (uint256 redemptionAssetAmount, uint256 ghoSold) = IGsm(GSM).buyAsset(minAmount, receiver);
+    IRedemption(REDEMPTION_CONTRACT).redeem(redemptionAssetAmount);
     IERC20(REDEEMED_ASSET).safeTransfer(receiver, redemptionAssetAmount);
 
     emit BuyAssetThroughRedemption(msg.sender, receiver, redemptionAssetAmount, ghoSold);
--- src/contracts/facilitators/gsm/dependencies/circle/interfaces/IRedemption.sol
+++ src/contracts/facilitators/gsm/dependencies/circle/interfaces/IRedemption.sol
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.10;
 
 /**
  * @title IRedemption
